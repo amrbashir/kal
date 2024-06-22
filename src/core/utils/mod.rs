ad9e5d2403be4c1ab3d93cdd50aedbfe18421b22
amrbashir <github@amrbashir.me>
2024-06-22 05:07:04 +0300

    Fix `IteratorExt::collect_non_empty` skipping first element

--- src/core/utils/mod.rs
+++ src/core/utils/mod.rs
@@ -14,7 +14,7 @@ pub trait IteratorExt: Iterator {
 impl<T: Iterator> IteratorExt for T {
     fn collect_non_empty<B: FromIterator<Self::Item>>(self) -> Option<B> {
         let mut peek = self.peekable();
-        peek.next().is_some().then(|| peek.collect())
+        peek.peek().is_some().then(|| peek.collect())
     }
 }
 
@@ -26,8 +26,16 @@ mod tests {
     fn it_collects_non_empty() {
         let empty = Vec::<u32>::new();
         let non_empty = [1];
+        let non_empty2 = [1, 2, 3];
 
         assert!(empty.iter().collect_non_empty::<Vec<_>>().is_none());
         assert!(non_empty.iter().collect_non_empty::<Vec<_>>().is_some());
+        assert_eq!(
+            non_empty2
+                .into_iter()
+                .collect_non_empty::<Vec<_>>()
+                .unwrap(),
+            vec![1, 2, 3]
+        );
     }
 }
