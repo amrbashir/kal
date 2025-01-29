de00609a432d0b2700cf9ff7eb6f43fe99d50f7d
amrbashir <github@amrbashir.me>
2025-01-29 04:36:27 +0200

    ci: split create installer from build

--- .scripts/build.ps1
+++ .scripts/build.ps1
@@ -1,13 +1,2 @@
 pnpm -r build
 cargo build --release
-
-if ($env:CARGO_TARGET_DIR) {
-  Copy-Item -Force "$env:CARGO_TARGET_DIR/release/kal.exe" "installer/kal.exe"
-} else {
-  Copy-Item -Force "./target/release/kal.exe" "installer/kal.exe"
-}
-
-makensis /V4 installer/installer.nsi
-
-Move-Item -Force "installer/kal.exe" "kal.exe"
-Move-Item -Force "installer/kal-setup.exe" "kal-setup.exe"
---
+++ .scripts/create-installer.ps1
@@ -0,0 +1,12 @@
+if ($env:CARGO_TARGET_DIR) {
+  Copy-Item -Force "$env:CARGO_TARGET_DIR/release/kal.exe" "installer/kal.exe"
+} else {
+  Copy-Item -Force "./target/release/kal.exe" "installer/kal.exe"
+}
+
+makensis /V4 installer/installer.nsi
+
+New-Item -Force "dist" -Type Directory > $null
+
+Move-Item -Force "installer/kal.exe" "dist/kal.exe"
+Move-Item -Force "installer/kal-setup.exe" "dist/kal-setup.exe"
