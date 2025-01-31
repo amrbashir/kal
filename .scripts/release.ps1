63067447f7a80b118c84a091586b589568ca09cb
amrbashir <github@amrbashir.me>
2025-01-31 21:12:12 +0200

    release: v0.2.0

--- .scripts/release.ps1
+++ .scripts/release.ps1
@@ -1,8 +1,10 @@
 $path = "kal/Cargo.toml"
-(Get-Content $path).replace("version = `"0.0.0`"", "version = `"$args`"") | Set-Content $path
+(Get-Content $path) -replace "version = `"[0-9].[0-9].[0-9]`"", "version = `"$args`"" | Set-Content $path
 
 $path = "kal-ui/package.json"
-(Get-Content $path).replace("`"version`": `"0.0.0`"", "`"version`": `"$args`"") | Set-Content $path
+(Get-Content $path) -replace "`"version`": `"[0-9].[0-9].[0-9]`"", "`"version`": `"$args`"" | Set-Content $path
+
+Start-Sleep -Seconds 2
 
 git add .
 git commit -m "release: v$args";
